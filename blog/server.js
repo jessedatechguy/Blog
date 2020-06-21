d4f8da765f58c7e7880d5e218cbaf53b495a3f33
jessedatechguy@gmail.com <Cl@ssof16>
2020-06-20 20:17:12 -0700

    working on server.js it is not linking.....

---
+++ blog/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = 5000;
+
+const app = express()
+
+app.use(express.static(__dirname + '/blog/BLOG'))
+
+app.get('/*', (req, res) => {
+    res.sendfile(path.join(__dirname));
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}!`)
+})
