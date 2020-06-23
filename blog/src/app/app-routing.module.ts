bc81fc439747ef40ac5ce394d8da05356d3f8c39
jessedatechguy@gmail.com <Cl@ssof16>
2020-06-23 15:54:01 -0700

    Taking a break adding the create a new blog page

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { CreateblogComponent } from './createblog/createblog.component';
 
 const routes: Routes = [
-  {path: 'articles', component: ArticleListComponent},
-  {path: '', component: ArticleListComponent}
+  { path: 'articles', component: ArticleListComponent},
+  { path: 'create', component:  CreateblogComponent}
+  { path: '', component: ArticleListComponent}
 ]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CreateblogComponent } from './createblog/createblog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    ArticleListComponent
+    ArticleListComponent,
+    CreateblogComponent
   ],
   imports: [
     BrowserModule,
