c1d7c91e497c0f4f929e112cf55f9847d81762fb
jessedatechguy@gmail.com <Cl@ssof16>
2020-06-28 08:05:52 -0700

    could not get or post my server.js file

--- blog/server.js
+++ blog/server.js
@@ -4,19 +4,25 @@ const path = require('path');
 const http = require('http');
 const app = express();
 
-const api = require('./server/api');
+// API file for interacting with MongoDB
+const api = require('./server/routes/api');
 
+// Parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
+// Angular DIST output folder
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// API location
 app.use('/api', api);
 
+// Send all other requests to the Angular app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/blog/index.html'));
 });
 
+//Set Port
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
--- blog/server/routes/api.js
+++ blog/server/routes/api.js
@@ -5,13 +5,14 @@ const ObjectID = require('mongodb').ObjectID;
 
 // Connect
 const connection = (closure) => {
-    return MongoClient.connect('mongodb://localhost:27017/mean', (err, db) => {
+    return MongoClient.connect('mongodb://localhost:27017/mean', {
+         useUnifiedTopology: true },
+        (err, db) => {
         if (err) return console.log(err);
 
         closure(db);
     });
 };
-
 
 // Error handling
 const sendError = (err, res) => {
@@ -27,13 +28,13 @@ let response = {
     message: null
 };
 
-// Get users
-router.get('/users', (req, res) => {
+// Get blog
+router.get('/blog', (req, res) => {
     connection((db) => {
-        db.collection('users')
+        db.collection('blog')
             .find()
             .toArray()
-            .then((users) => {
+            .then((blog) => {
                 response.data = users;
                 res.json(response);
             })
